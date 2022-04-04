27d9d5ff98a7693e025ed3e876809ca8c8898a23
trojanova <100875276+trojanova@users.noreply.github.com>
2022-04-04 19:20:58 +0200

    Zjednoduseni scriptu pro tabulku dodatecnych informaci

--- Engeto_SQL_project_script.sql
+++ Engeto_SQL_project_script.sql
@@ -27,41 +27,12 @@ CREATE TABLE t_tereza_trojanova_project_sql_secondary_final (
 	SELECT
 		c.country, 
 		c.abbreviation, 
-		c.avg_height, 
-		c.calling_code, 
-		c.capital_city, 
-		c.continent, 
-		c.currency_name, 
-		c.religion, 
-		c.currency_code, 
-		c.domain_tld, 
-		c.elevation, 
-		c.north, south, 
-		c.west, 
-		c.east, 
-		c.government_type, 
-		c.independence_date, 
-		c.iso_numeric, 
-		c.landlocked, 
-		c.life_expectancy, 
-		c.national_symbol, 
-		c.national_dish, 
-		c.population_density, 
-		c.population, 
-		c.region_in_world, 
-		c.surface_area, 
-		c.yearly_average_temperature, 
-		c.median_age_2018, 
-		c.iso2, 
-		c.iso3,
 		e.`year`, 
 		e.GDP, 
-		e.gini, 
-		e.taxes, 
-		e.fertility, 
-		e.mortaliy_under5
+		e.gini
 	FROM countries c
 	LEFT JOIN economies e ON c.country = e.country
+	WHERE e.`year` BETWEEN 2006 AND 2018
 );
 
 
